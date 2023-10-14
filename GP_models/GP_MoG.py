60f91c1cbf1c47762c0dd8558398703d1f8b705f
josephinecowley <josephinecowley1010@gmail.com>
2023-10-14 18:38:02 +0100

    feat: add optimisation of MoG kernel

--- GP_models/GP_MoG.py
+++ GP_models/GP_MoG.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 
+from scipy.optimize import minimize
+from scipy.linalg import solve_triangular
+from numpy.linalg import cholesky, det
+
 
 def plot_gp(mu, cov, X, X_train=None, Y_train=None, samples=[]):
     X = X.ravel()
@@ -31,13 +35,13 @@ def MoG_spectral_kernel(x1, x2, M=3,  sigma=0.1, frequencies=[440]):
     return (1/np.pi ) * np.exp(-(sigma**2/2) * np.linalg.norm(x1- x2)**2) * cosine_series
 
 
-def plot_cov_matrix(X, Y=None):
+def plot_cov_matrix(X, Y=None, M=3,  sigma=0.1, frequencies=[40]):
     if Y is None:
         Y = X
     cov = np.zeros((len(X), len(Y)))
     for i in range(len(X)):
         for j in range(len(Y)):
-            cov[i,j] = MoG_spectral_kernel(i, j)
+            cov[i,j] = MoG_spectral_kernel(i, j, M=M, sigma=sigma, frequencies=frequencies)
     return cov
 
 
@@ -59,7 +63,7 @@ plot_gp(mu, cov, X, samples=samples)
 plt.show()
 
 
-def posterior(X_s, X_train, Y_train,  M=10, sigma=5., frequency=265, sigma_y=1e-8): 
+def posterior(X_s, X_train, Y_train, M=3, sigma=0.1,  sigma_y=1e-8): 
     """
     Computes the suffifient statistics of the posterior distribution 
     from m training data X_train and Y_train and n new inputs X_s.
@@ -75,10 +79,10 @@ def posterior(X_s, X_train, Y_train,  M=10, sigma=5., frequency=265, sigma_y=1e-
     Returns:
         Posterior mean vector (n x d) and covariance matrix (n x n).
     """
-    K = plot_cov_matrix(X_train, X_train) + \
+    K = plot_cov_matrix(X_train, X_train, M=M, sigma = sigma) + \
         sigma_y**2 * np.eye(len(X_train))
-    K_s = plot_cov_matrix(X_train, X_s)
-    K_ss = plot_cov_matrix(X_s, X_s) + 1e-8 * np.eye(len(X_s))
+    K_s = plot_cov_matrix(X_train, X_s,M=M, sigma = sigma)
+    K_ss = plot_cov_matrix(X_s, X_s,M=M, sigma = sigma) + 1e-8 * np.eye(len(X_s))
     K_inv = inv(K)
 
     # Equation (7)
@@ -103,8 +107,8 @@ Y_train = data[:200].reshape(-1, 1)  # Truncate data to make manageable
 time_length = Y_train.shape[0] / sample_rate
 
 # # Plotting the wave form in the time domain
-X_train = np.linspace(0., 10, Y_train.shape[0]).reshape(-1, 1) # Had to change time_length to 10 inorder to see changes in kernel function
-X = np.linspace(0, 10, 200).reshape(-1, 1) # For some reason I need to make this the same shape as the training data - need to look into this
+X_train = np.linspace(0., time_length, Y_train.shape[0]).reshape(-1, 1) # Had to change time_length to 10 inorder to see changes in kernel function
+X = np.linspace(0, time_length, 100).reshape(-1, 1) # For some reason I need to make this the same shape as the training data - need to look into this
 
 
 
@@ -120,8 +124,77 @@ noise = 0.4
 
 # Compute mean and covariance of the posterior distribution
 
-mu_s, cov_s = posterior(X, X_train, Y_train, sigma = 3, sigma_y=noise, M=20, frequency = 4400)
+mu_s, cov_s = posterior(X, X_train,Y_train, M = 3, sigma=0.1, sigma_y=noise)
 
 samples = np.random.multivariate_normal(mu_s.ravel(), cov_s, 3)
 plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train, samples=samples)
+plt.show()
+
+
+def nll_fn(X_train, Y_train, noise, naive=True):
+    """
+    Returns a function that computes the negative log marginal
+    likelihood for training data X_train and Y_train and given
+    noise level.
+
+    Args:
+        X_train: training locations (m x d).
+        Y_train: training targets (m x 1).
+        noise: known noise level of Y_train.
+        naive: if True use a naive implementation of Eq. (11), if
+               False use a numerically more stable implementation.
+
+    Returns:
+        Minimization objective.
+    """
+    
+    Y_train = Y_train.ravel()
+    
+    def nll_naive(theta):
+        # Naive implementation of Eq. (11). Works well for the examples 
+        # in this article but is numerically less stable compared to 
+        # the implementation in nll_stable below.
+        K = plot_cov_matrix(X_train, X_train, M=int(theta[0]), sigma=theta[1]) + \
+            noise**2 * np.eye(len(X_train))
+        return 0.5 * np.log(det(K)) + \
+               0.5 * Y_train.dot(inv(K).dot(Y_train)) + \
+               0.5 * len(X_train) * np.log(2*np.pi)
+        
+    def nll_stable(theta):
+        # Numerically more stable implementation of Eq. (11) as described
+        # in http://www.gaussianprocess.org/gpml/chapters/RW2.pdf, Section
+        # 2.2, Algorithm 2.1.
+        
+        K = plot_cov_matrix(X_train, X_train, M=int(theta[0]), sigma=theta[1]) + \
+            noise**2 * np.eye(len(X_train))
+        L = cholesky(K)
+        
+        S1 = solve_triangular(L, Y_train, lower=True)
+        S2 = solve_triangular(L.T, S1, lower=False)
+        
+        return np.sum(np.log(np.diagonal(L))) + \
+               0.5 * Y_train.dot(S2) + \
+               0.5 * len(X_train) * np.log(2*np.pi)
+
+    if naive:
+        return nll_naive
+    else:
+        return nll_stable
+    
+    
+# Minimize the negative log-likelihood w.r.t. parameters l and sigma_f.
+# We should actually run the minimization several times with different
+# initializations to avoid local minima but this is skipped here for
+# simplicity.
+res = minimize(nll_fn(X_train, Y_train, noise), [3, 0.1], 
+               bounds=((1e-5, None), (1e-5, None)),
+               method='L-BFGS-B')
+
+# Store the optimization results in global variables so that we can
+# compare it later with the results from other implementations.
+m, sigma_opt = res.x
+print(m, sigma_opt)
+# Compute posterior mean and covariance with optimized kernel parameters and plot the results
+mu_s, cov_s = posterior(X, X_train, Y_train, M=int(m), sigma=sigma_opt, sigma_y=noise)
+plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train)
 plt.show()
