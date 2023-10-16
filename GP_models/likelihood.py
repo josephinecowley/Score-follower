033c4642af98c4d06912da3027e88b2136e4978a
josephinecowley <josephinecowley1010@gmail.com>
2023-10-17 00:13:19 +0100

    feat: add negative log likelihood function

--- GP_models/likelihood.py
+++ GP_models/likelihood.py
@@ -1,179 +1,153 @@
+from numpy.linalg import inv
 import numpy as np
 import matplotlib.pyplot as plt
-from gaussian_processes_util import plot_gp
+import scipy.io.wavfile as wav
 from numpy.linalg import inv
 from numpy.linalg import cholesky, det
 from scipy.linalg import solve_triangular
 from scipy.optimize import minimize
-import scipy.io.wavfile as wav
-import GP_models.GPy_likelihood as GPy_likelihood
+from scipy.fft import fft
+from scipy.fft import fftfreq
+
+def plot_gp(mu, cov, X, X_train=None, Y_train=None, samples=[]):
+    X = X.ravel()
+    mu = mu.ravel()
+    uncertainty = 1.96 * np.sqrt(np.diag(cov)) # Plot 95% curves
+
+    plt.fill_between(X, mu + uncertainty, mu - uncertainty, alpha=0.1)
+    plt.plot(X, mu, label='Mean')
+    for i, sample in enumerate(samples):
+        plt.plot(X, sample, lw=1, ls='--', label=f'Sample {i+1}')
+    if X_train is not None:
+        plt.plot(X_train, Y_train, 'rx')
+    plt.legend()
 
 
-
-def kernel(X1, X2, l=1.0, sigma_f=1.0):
+def MoG_spectral_kernel_matrix(X1, X2, M=10, sigma=100000, frequencies=[600, 1200]):
     """
-    Isotropic squared exponential kernel.
+    Compute the MoG spectral kernel matrix between two sets of vectors.
     
-    Args:
-        X1: Array of m points (m x d).
-        X2: Array of n points (n x d).
-
-    Returns:
-        (m x n) matrix.
+    X1 and X2 are arrays of shape (n1, d) and (n2, d), where n1 and n2 are the
+    numbers of vectors and d is the dimension of each vector.
+    
+    M is the number of partials or harmonics for each note source.
     """
-    sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-    return sigma_f**2 * np.exp((-0.5 / l**2) * sqdist)
+    n1= X1.shape[0]
+    n2 = X2.shape[0]
+    
+    kernel_matrix = np.zeros((n1, n2))
+    
+    for i in range(n1):
+        for j in range(n2):
+            x1 = X1[i, :]
+            x2 = X2[j, :]
+            
+            cosine_series = 0
+            for fundamental_frequency in frequencies:
+                for m in range(M):
+                    cosine_series += np.cos((m + 1) * 2 * np.pi * fundamental_frequency * np.linalg.norm(x1 - x2))
+            
+            kernel_value = (1 / np.pi) * np.exp(-(sigma**2 / 2) * np.linalg.norm(x1 - x2)**2) * cosine_series
+            kernel_matrix[i, j] = kernel_value
+    
+    return kernel_matrix
 
-# def periodic_kernel(X1, X2, l=1.0, sigma_f=1.0, frequency=440): WIP
-#     """
-#     Periodic kernel
-#     """
-#     base =  np.pi * (X1[:, None, :] - X2[None, :, :]) * frequency
-#     return np.square(sigma_f)  * np.exp( -2* np.sum( np.square(  np.sin( base ) / l ), axis = -1 ) )
-
-# Finite number of points
-X = np.arange(-5, 5, 0.2).reshape(-1, 1)
-
-# Mean and covariance of the prior
-mu = np.zeros(X.shape)
-cov = kernel(X, X)
-
-# Plot heat map of covariance function
-plt.imshow(cov, cmap='hot', interpolation='nearest')
-plt.show()
-
-# Draw three samples from the prior
-samples = np.random.multivariate_normal(mu.ravel(), cov, 3)
-
-# Plot GP mean, uncertainty region and samples 
-plot_gp(mu, cov, X, samples=samples)
-
-
-
-def posterior(X_s, X_train, Y_train, l=1.0, sigma_f=1.0, sigma_y=1e-8):
+def posterior(X_s, X_train, Y_train, M=10, sigma=1.0, sigma_y=1e-8, frequencies = [440]): # for RBF,viola, l = 0.00005 and sigma_f = 2 are optimal
     """
-    Computes the suffifient statistics of the posterior distribution 
+    Computes the sufficient statistics of the posterior distribution 
     from m training data X_train and Y_train and n new inputs X_s.
-    
+
     Args:
         X_s: New input locations (n x d).
         X_train: Training locations (m x d).
         Y_train: Training targets (m x 1).
-        l: Kernel length parameter.
-        sigma_f: Kernel vertical variation parameter.
+        M: Number of partials.
+        sigma: Kernel vertical variation parameter.
         sigma_y: Noise parameter.
-    
+
     Returns:
         Posterior mean vector (n x d) and covariance matrix (n x n).
     """
-    K = kernel(X_train, X_train, l, sigma_f) + sigma_y**2 * np.eye(len(X_train))
-    K_s = kernel(X_train, X_s, l, sigma_f)
-    K_ss = kernel(X_s, X_s, l, sigma_f) + 1e-8 * np.eye(len(X_s))
+    K = MoG_spectral_kernel_matrix(X_train, X_train, M=M, sigma=sigma, frequencies=frequencies) + \
+       sigma_y**2 * np.eye(len(X_train))
+    K_s = MoG_spectral_kernel_matrix(X_train, X_s, M=M, sigma=sigma, frequencies=frequencies)
+    K_ss = MoG_spectral_kernel_matrix(X_s, X_s, M=M, sigma=sigma, frequencies=frequencies) + 1e-8 * np.eye(len(X_s))
     K_inv = inv(K)
-    
-    # Equation (7)
+
+    # Calculate posterior mean function
     mu_s = K_s.T.dot(K_inv).dot(Y_train)
 
-    # Equation (8)
+    # Calculate posterior covariance function
     cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
-    
+
     return mu_s, cov_s
 
-noise = 0.4
 
-# # Option 1: artifical data
-# # Noisy training data
-# X_train = np.arange(-3, 4, 1).reshape(-1, 1)
-# Y_train = np.sin(X_train) + noise * np.random.randn(*X_train.shape)
-
-# Option 2: wav data
-# Read a WAV file
+# Wav file method
 wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/viola_octave.wav'
+
+# Read a Wav file
 sample_rate, data = wav.read(wav_file)
 
 # Truncate data to make manageable
-Y_train = data[:50].reshape(-1, 1)  
+Y_train = data[:100].reshape(-1, 1)  
 
-# Find time time length of truncated data
-time_length = 10000 *Y_train.shape[0] / sample_rate
+# Find time length of truncated data
+time_length = Y_train.shape[0] / sample_rate
 
-# Plot times
+# # Plotting the wave form in the time domain
 X_train = np.linspace(0., time_length, Y_train.shape[0]).reshape(-1, 1)
-
-# Plot new points to be estimated
 X = np.linspace(0, time_length, 200).reshape(-1, 1) 
 
+noise = 0.0005
+
 # Compute mean and covariance of the posterior distribution
-mu_s, cov_s = posterior(X, X_train, Y_train, sigma_y=noise)
+mu_s, cov_s = posterior(X, X_train, Y_train, M = 20,sigma_y=noise)
 
 samples = np.random.multivariate_normal(mu_s.ravel(), cov_s, 3)
 plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train, samples=samples)
+plt.show()
 
+# # Convert audio data to frequency domain
+# fft_data = fft(mu_s)
+# N = len(mu_s)
+# normalise = N/2
 
+# # Get the frequency components of the spectrum
+# frequency_axis = fftfreq(N, d=1/44100)
+# norm_amplitude = np.abs(fft_data)/normalise
 
-def nll_fn(X_train, Y_train, noise, naive=True):
-    """
-    Returns a function that computes the negative log marginal
-    likelihood for training data X_train and Y_train and given
-    noise level.
+# # Plot the results
+# plt.plot(frequency_axis, norm_amplitude)
+# plt.xlabel('Frequency[Hz]')
+# plt.ylabel('Amplitude')
+# plt.title('Spectrum')
+# plt.show()
 
-    Args:
-        X_train: training locations (m x d).
-        Y_train: training targets (m x 1).
-        noise: known noise level of Y_train.
-        naive: if True use a naive implementation of Eq. (11), if
-               False use a numerically more stable implementation.
-
-    Returns:
-        Minimization objective.
-    """
-    
+def log_likelihood(X_train, Y_train, M=10, sigma=100, frequencies=[400]):
     Y_train = Y_train.ravel()
-    
-    def nll_naive(theta):
-        # Naive implementation of Eq. (11). Works well for the examples 
-        # in this article but is numerically less stable compared to 
-        # the implementation in nll_stable below.
-        K = kernel(X_train, X_train, l=theta[0], sigma_f=theta[1]) + \
-            noise**2 * np.eye(len(X_train))
-        return 0.5 * np.log(det(K)) + \
-               0.5 * Y_train.dot(inv(K).dot(Y_train)) + \
-               0.5 * len(X_train) * np.log(2*np.pi)
-        
-    def nll_stable(theta):
-        # Numerically more stable implementation of Eq. (11) as described
-        # in http://www.gaussianprocess.org/gpml/chapters/RW2.pdf, Section
-        # 2.2, Algorithm 2.1.
-        
-        K = kernel(X_train, X_train, l=theta[0], sigma_f=theta[1]) + \
-            noise**2 * np.eye(len(X_train))
-        L = cholesky(K)
-        
-        S1 = solve_triangular(L, Y_train, lower=True)
-        S2 = solve_triangular(L.T, S1, lower=False)
-        
-        return np.sum(np.log(np.diagonal(L))) + \
-               0.5 * Y_train.dot(S2) + \
-               0.5 * len(X_train) * np.log(2*np.pi)
+    K = MoG_spectral_kernel_matrix(X_train, X_train, M=M, sigma=sigma, frequencies=frequencies) + \
+        noise**2 * np.eye(len(X_train))
+    print(det(K))
+    return 0.5 + 0.5 * Y_train.dot(inv(K).dot(Y_train)) + 0.5 * len(X_train) * np.log(2*np.pi)
 
-    if naive:
-        return nll_naive
-    else:
-        return nll_stable
+def stable_log_likelihood(X_train, Y_train, M=10, sigma=100, frequencies=[400]):
+    Y_train = Y_train.ravel()
+    K = MoG_spectral_kernel_matrix(X_train, X_train, M=M, sigma=sigma, frequencies=frequencies) + \
+        noise**2 * np.eye(len(X_train))
+    L = cholesky(K)
     
+    S1 = solve_triangular(L, Y_train, lower=True)
+    S2 = solve_triangular(L.T, S1, lower=False)
     
-# Minimize the negative log-likelihood w.r.t. parameters l and sigma_f.
-# We should actually run the minimization several times with different
-# initializations to avoid local minima but this is skipped here for
-# simplicity.
-res = minimize(nll_fn(X_train, Y_train, noise), [1, 1], 
-               bounds=((1e-5, None), (1e-5, None)),
+    return np.sum(np.log(np.diagonal(L))) + \
+            0.5 * Y_train.dot(S2) + \
+            0.5 * len(X_train) * np.log(2*np.pi)
+
+print(f"loglikelihood of 586 Hz is {stable_log_likelihood(X_train=X_train, Y_train=Y_train, frequencies=[586])}")
+print(f"loglikelihood of 440 Hz is {stable_log_likelihood(X_train=X_train, Y_train=Y_train, frequencies=[440])}")
+print(f"loglikelihood of 5 Hz is {stable_log_likelihood(X_train=X_train, Y_train=Y_train, frequencies=[5])}")
+
+res = minimize(stable_log_likelihood(X_train, Y_train, noise),  [frequencies=[440]] , 
+               bounds=((20,20000)),
                method='L-BFGS-B')
-
-# Store the optimization results in global variables so that we can
-# compare it later with the results from other implementations.
-l_opt, sigma_f_opt = res.x
-
-# Compute posterior mean and covariance with optimized kernel parameters and plot the results
-mu_s, cov_s = posterior(X, X_train, Y_train, l=l_opt, sigma_f=sigma_f_opt, sigma_y=noise)
-plot_gp(mu_s, cov_s, X, X_train=X_train, Y_train=Y_train)
