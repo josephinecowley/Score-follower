e8d11134178e07a50e90ce4294ed1e268238017a
josephinecowley <josephinecowley1010@gmail.com>
2023-10-24 14:49:28 +0100

    feat: update the trial to change priors

--- torch/trial.py
+++ torch/trial.py
@@ -18,21 +18,27 @@ y_train = torch.sin(x_train * (2 * frequency * math.pi)) + \
 plt.plot(x_train.numpy(), y_train.numpy(), 'k*')
 plt.show()
 
+
 # Initialise the likelihood and model
 likelihood = gpytorch.likelihoods.GaussianLikelihood(
     noise_constraint=gpytorch.constraints.GreaterThan(1e-6))
 model = models.SpectralMixtureGP(x_train, y_train, likelihood)
-
+# model.cov.mixture_means = torch.tensor([[325.8866], [4.4211], [440.0000]])
+# model.cov.mixture_scales = torch.tensor([[5.0000], [0.0005], [0.000005]])
+# model.cov.mixture_weights = torch.tensor([[0.1426], [0.1246], [0.126]])
+convenience_functions.plot_spectral_density(model.spectral_density(model.cov))
+print(model.cov.mixture_means.detach().reshape(-1, 1),
+      model.cov.mixture_scales.detach().reshape(-1, 1), model.cov.mixture_weights.detach())
 # Update hyperparameters
 hypers = {
-    'likelihood.noise_covar.noise': torch.tensor(0.000001),
+    'likelihood.noise_covar.noise': torch.tensor(0.000001)
 }
 model.initialize(**hypers)
 mll = gpytorch.mlls.ExactMarginalLogLikelihood(model.likelihood, model)
 
 # Print loglikelihood of prior
 loss = -mll(model(x_train), y_train)
-print("MLL after training", loss.detach().cpu().item())
+# print("MLL after training", loss.detach().cpu().item())
 
 # Train model
 model, loss = train.train_with_restarts(model, 50, 10)
