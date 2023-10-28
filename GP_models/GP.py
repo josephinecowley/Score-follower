742973ccb3d76086607e2272b1798afe9d716748
josephinecowley <josephinecowley1010@gmail.com>
2023-10-28 21:00:29 +0100

    feat: coefficient for speknernel optimised for piano 440

--- GP_models/GP.py
+++ GP_models/GP.py
@@ -10,30 +10,28 @@ import helper
 # ---------------------
 
 # Wav file method
-wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/tuner_440.wav'
+wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/A_440_piano.wav'
 
 # Read a Wav file
 sample_rate, data = wav.read(wav_file)
 
 # Truncate data to 700 samples long
-data = data[100:1000]
+data = data[500:2000]
 audio_duration = len(data)/sample_rate
 time_samples = np.linspace(0, audio_duration, len(data))
 
 # helper.plot_fft(data, sample_rate, colour='k')
 
 # ---------------------
-# Plotting out frequency spectrum of kernel directly
+# Plotting out frequency spectrum of kernel power spectrum directly
 # ---------------------
 
 helper.plot_fft(data, sample_rate)
 output, f_spectrum = helper.return_kernel_spectrum(
-    f=[440], M=8, sigma_f=30,  max_freq=20000, no_samples=len(data))  # sigma_f allows the gps to lookthe same height more or less....
+    f=[440], M=8, sigma_f=20,  max_freq=20000, no_samples=len(data))  # sigma_f allows the gps to lookthe same height more or less....
 # sqrt because it was the power spectrum we created
-plt.plot(f_spectrum, 9*np.sqrt(output))
+plt.plot(f_spectrum, 69*np.sqrt(output))
 plt.show()
-
-# helper.plot_fft(data, sample_rate)
 
 
 # ---------------------
