e433684e82ea4e99c881f5d9eac7c21ed2a3ffc0
josephinecowley <josephinecowley1010@gmail.com>
2023-10-31 15:26:03 +0000

    feat: improve note onsret detections

--- GP_models/onset_detections.py
+++ GP_models/onset_detections.py
@@ -2,24 +2,27 @@ import librosa
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as wav
 import helper
-
-data, sr = librosa.load(
-    '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/scale.wav')
-
-onset_frames = librosa.onset.onset_detect(
-    y=data, sr=sr, wait=1, pre_avg=1, post_avg=1, pre_max=1, post_max=1)
-onset_times = librosa.frames_to_time(onset_frames)
-
-
-print(onset_times)
+import numpy as np
 
 
 wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/scale.wav'
 sample_rate, data = wav.read(wav_file)
+onset_times = librosa.onset.onset_detect(
+    y=data, sr=sample_rate, wait=0.01, pre_avg=1, post_avg=1, pre_max=1, post_max=1, units='time')
+
+audio_duration = len(data)/sample_rate
+time_samples = np.linspace(0, audio_duration, len(data))
 onset_time_samples = onset_times * sample_rate
 
+sample_data = []
+
+helper.plot_audio(time_samples, data)
+for time in onset_times:
+    plt.vlines(time, ymin=-2, ymax=2, colors='blue', zorder=2)
+plt.show()
+
 for i in range(len(onset_time_samples)):
-    sample_data = data[int(onset_time_samples[i])
-                           :int(onset_time_samples[i])+1000]
-    helper.plot_fft(sample_data, sample_rate)
+    print(int(onset_time_samples[i]))
+    sample_data.append(data[int(onset_time_samples[i]):int(onset_time_samples[i])+10000])
+    helper.plot_fft(sample_data[i], sample_rate)
     plt.show()
