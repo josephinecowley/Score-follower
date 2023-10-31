5a156000e54b8a7e9024eea79a277cc8c6965cb0
josephinecowley <josephinecowley1010@gmail.com>
2023-10-31 16:33:36 +0000

    feat: further updates

--- GP_models/onset_detections.py
+++ GP_models/onset_detections.py
@@ -5,18 +5,16 @@ import helper
 import numpy as np
 from librosa import note_to_hz as hz
 
-# scale_freqs = [[262], [294], [330], [349], [392], [440], [494], [523]]
 scale_freqs = hz([['C4'], ['D4'], ['E4'], ['F4'],
                  ['G4'], ['A4'], ['B4'], ['C5']])
 beethoven_freqs = hz([['E4', 'G#4', 'B4', 'E5', 'G#5'], [
                      'B#3', 'D#4', 'F#4', 'A4', 'D#5', 'F#5']])
-print(beethoven_freqs)
 
 
-wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/scale.wav'
+wav_file = '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/wav_files/beethoven.wav'
 sample_rate, data = wav.read(wav_file)
 onset_times = librosa.onset.onset_detect(
-    y=data, sr=sample_rate, wait=0.01, pre_avg=1, post_avg=1, pre_max=1, post_max=1, units='time')
+    y=data, post_avg=5, wait=1,  sr=sample_rate, units='time')  # delta=0.15, wait=5, pre_avg=0, post_avg=5, pre_max=5, post_max=10,
 
 audio_duration = len(data)/sample_rate
 time_samples = np.linspace(0, audio_duration, len(data))
@@ -31,7 +29,8 @@ plt.show()
 
 for i in range(len(onset_time_samples)):
     print(int(onset_time_samples[i]))
-    sample_data.append(data[int(onset_time_samples[i]):int(onset_time_samples[i])+10000])
+    sample_data.append(data[int(onset_time_samples[i])
+                       :int(onset_time_samples[i])+10000])
     helper.plot_fft(sample_data[i], sample_rate)
-    helper.return_kernel_spectrum(f=scale_freqs[i], show=True, scalar=50)
+    helper.return_kernel_spectrum(f=beethoven_freqs[i], show=True, scalar=50)
     plt.show()
