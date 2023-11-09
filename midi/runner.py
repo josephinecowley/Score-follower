55acc12fc551bf6fbd43cb242f23a8018012d15d
josephinecowley <josephinecowley1010@gmail.com>
2023-11-09 13:55:35 +0000

    feat: add midi utilisation

---
+++ midi/runner.py
@@ -0,0 +1,4 @@
+from midi import process_midi_to_note_info
+
+ret = process_midi_to_note_info(
+    '/Users/josephine/Documents/Engineering /Part IIB/Score alignment project/Score-follower/midi_files/waldstein_1.midi')
