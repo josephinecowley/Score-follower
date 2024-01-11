8c6ad599820edf240e5f4f13a0a62c451e0b82c5
josephinecowley <josephinecowley1010@gmail.com>
2024-01-11 22:07:58 +0000

    feat: update constants to have GP hyperparameters

--- lib/constants.py
+++ lib/constants.py
@@ -4,3 +4,11 @@ DEFAULT_SAMPLE_RATE = 44100
 DEFAULT_FRAME_LENGTH = 1000
 
 REPO_ROOT = os.path.dirname(os.path.dirname(__file__))
+
+# GP Hyperparameters
+DEFAULT_M = 9
+DEFAULT_SIGMA_F = 0.1
+DEFAULT_SIGMA_N = 0.01
+
+DEFAULT_V = 2.37
+DEFAULT_T = 0.465
