ba0ec49f4e756b8975aa4ff3c086e092929b3327
josephinecowley <josephinecowley1010@gmail.com>
2024-01-14 00:51:59 +0000

    feat: added feature extraction

--- lib/components/audiopreprocessor.py
+++ lib/components/audiopreprocessor.py
@@ -47,7 +47,6 @@ class Slicer:
         for s in audio_stream:
             pre_sleep_time = time.perf_counter()
             self.slice_queue.put(s)
-            print(s)
             # sleep for hop length
             self.__sleep(self.hop_length, pre_sleep_time)
 
@@ -66,6 +65,40 @@ class Slicer:
         eprint(f"[{self.__class__.__name__}] {msg}")
 
 
+class FeatureExtractor:
+    def __init__(
+            self,
+            slice_queue: ExtractedFeatureQueue,
+            output_queue: ExtractedFeatureQueue,
+            hop_length: int,
+            frame_length: int,
+            sample_rate: int,
+    ):
+        self.slice_queue = slice_queue
+        self.output_queue = output_queue
+        self.hop_length = hop_length
+        self.frame_length = frame_length
+        self.sample_rate = sample_rate
+
+        self.__log("Initialised successfully")
+
+    def start(self):
+        self.__log("Starting...")
+        while True:
+            sl: Optional[np.ndarray] = self.slice_queue.get()
+            if sl is None:
+                break
+            self.output_queue.put(sl)
+        self.output_queue.put(None)  # end
+        self.__log("Finished")
+
+    def __log(self, msg: str):
+        eprint(f"[{self.__class__.__name__}] {msg}")
+
+
+# TODO continue!@!!
+
+
 class AudioPreprocessor:
     def __init__(
         self,
@@ -80,7 +113,7 @@ class AudioPreprocessor:
     ):
         self.sample_rate = sample_rate
         self.hop_length = hop_length
-        self.frame_len = frame_length
+        self.frame_length = frame_length
         self.wave_path = wave_path
         self.sleep_compensation = sleep_compensation
         self.output_queue = output_queue
@@ -96,7 +129,7 @@ class AudioPreprocessor:
         slicer = Slicer(
             wave_path=self.wave_path,
             hop_length=self.hop_length,
-            frame_length=self.frame_len,
+            frame_length=self.frame_length,
             sample_rate=self.sample_rate,
             slice_queue=slice_queue,
             sleep_compensation=self.sleep_compensation,
@@ -104,7 +137,19 @@ class AudioPreprocessor:
 
         online_slicer_proc = mp.Process(target=slicer.start)
         online_slicer_proc.start()
+
+        feature_extractor = FeatureExtractor(
+            slice_queue=slice_queue,
+            output_queue=self.output_queue,
+            hop_length=self.hop_length,
+            frame_length=self.frame_length,
+            sample_rate=self.sample_rate,
+        )
+        feature_extractor_proc = mp.Process(target=feature_extractor.start)
+        feature_extractor_proc.start()
+
         online_slicer_proc.join()
+        feature_extractor_proc.join()
         self.__log("Finished")
 
     def __log(self, msg: str):
