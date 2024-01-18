c1c73acc96d3f77746e00d053df4c38331d3cd3e
josephinecowley <josephinecowley1010@gmail.com>
2024-01-18 22:46:36 +0000

    feat: make it possible to have no performance recording option. In this situation, the player will not play, and we will instead listen for live music

--- lib/components/audiopreprocessor.py
+++ lib/components/audiopreprocessor.py
@@ -3,7 +3,7 @@ from ..mputils import write_list_to_queue
 from ..eprint import eprint
 from sharedtypes import AudioFrame, AudioFrameQueue
 from typing import Callable, Optional, Dict, List, Union
-
+import sounddevice as sd
 import multiprocessing as mp
 import librosa  # type: ignore
 import time
@@ -13,7 +13,7 @@ import numpy as np
 class Slicer:
     def __init__(
         self,
-        wave_path: str,
+        wave_path: Optional[str],
         hop_length: int,
         frame_length: int,
         sample_rate: int,
@@ -30,28 +30,35 @@ class Slicer:
         self.__log("Initialised successfully")
 
     def start(self):
-        self.__log("Starting...")
-        audio_stream = librosa.stream(
-            path=self.wave_path,
-            block_length=1,
-            frame_length=self.frame_length,
-            hop_length=self.hop_length,
-            mono=True,
-            fill_value=0,
-        )
+        if self.wave_path:  # If htere is a wave path, we will not do live return
+            self.__log("Starting using performance recording...")
+            audio_stream = librosa.stream(
+                path=self.wave_path,
+                block_length=1,
+                # TODO hop length parameter didn't seem to be working so this is a current fix
+                frame_length=self.hop_length,
+                hop_length=self.hop_length,
+                mono=True,
+                fill_value=0,
+            )
 
-        # before starting, sleep for frame_length
-        self.__sleep(
-            self.frame_length, time.perf_counter() + 0.2)
+            # before starting, sleep for frame_length
+            self.__sleep(
+                self.frame_length, time.perf_counter() + 0.2)
 
-        for s in audio_stream:
-            pre_sleep_time = time.perf_counter()
-            self.audio_frames_queue.put(s)
-            # sleep for hop length
-            self.__sleep(self.hop_length, pre_sleep_time)
+            for audio_frame in audio_stream:
+                pre_sleep_time = time.perf_counter()
+                self.audio_frames_queue.put(audio_frame[:self.frame_length])
+                # sleep for hop length
+                self.__sleep(self.hop_length, pre_sleep_time)
 
-        self.audio_frames_queue.put(None)  # end
-        self.__log("Finished")
+            self.audio_frames_queue.put(None)  # end
+            self.__log("Finished")
+
+        else:
+            self.__log("Starting to listen...")
+            self.audio_frames_queue.put(None)  # end
+            self.__log("Finished")
 
     def __sleep(self, samples: int, pre_sleep_time: float):
         sleep_time = float(samples) / self.sample_rate
@@ -72,7 +79,7 @@ class AudioPreprocessor:
         hop_length: int,
         frame_length: int,
         # slicer
-        wave_path: str,
+        wave_path: Optional[str],
         sleep_compensation: float,
         audio_frames_queue: AudioFrameQueue,
 
