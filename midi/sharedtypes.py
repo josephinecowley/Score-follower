bb38ac84a549827062c771ae47715a9bed3a254a
josephinecowley <josephinecowley1010@gmail.com>
2024-02-04 12:13:52 +0000

    feat: updated shared types to add note duration information into our data structure

--- midi/sharedtypes.py
+++ midi/sharedtypes.py
@@ -9,6 +9,7 @@ class NoteInfo:
     midi_note_num: int  # MIDI note number
     note_start: float  # note start time (ms)
     note_end: float = None  # note end time (ms)
+    note_duration: float = None  # Note duration
 
     def __eq__(self, other):
         if not isinstance(other, NoteInfo):
@@ -18,17 +19,17 @@ class NoteInfo:
             self.midi_note_num == other.midi_note_num
             and self.note_start == other.note_start
             and self.note_end == other.note_end
+            and self.note_duration == other.note_end - other.note_start
         )
 
     def __hash__(self):
-        return hash((self.midi_note_num, self.note_start, self.note_end))
+        return hash((self.midi_note_num, self.note_start, self.note_end, self.note_duration))
 
 
-ExtractedFeature = NewType("ExtractedFeature", np.ndarray)  # type: ignore
-ExtractedFeatureQueue = NewType(
-    "ExtractedFeatureQueue", "mp.Queue[Optional[ExtractedFeature]]"
+AudioFrame = NewType("AudioFrame", np.ndarray)  # type: ignore
+AudioFrameQueue = NewType(
+    "AudioFrameQueue", "mp.Queue[Optional[AudioFrame]]"
 )
-ModeType = Literal["online", "offline"]
 
 FollowerOutputQueue = NewType(
     "FollowerOutputQueue", "mp.Queue[Optional[DTWPathElemType]]"
@@ -36,3 +37,5 @@ FollowerOutputQueue = NewType(
 MultiprocessingConnection = NewType(
     "MultiprocessingConnection", "mp.connection.Connection"
 )
+
+Mode = Literal["basic", "oltw", "HMM"]
