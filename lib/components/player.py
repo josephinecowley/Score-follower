b839e4e5a2a870ca06622d26c340e3ce6eef8970
josephinecowley <josephinecowley1010@gmail.com>
2024-02-24 12:54:29 +0000

    feat: add player delay in Player class to make up for misalignment when using a pre-recorded piece

--- lib/components/player.py
+++ lib/components/player.py
@@ -1,5 +1,6 @@
 from ..eprint import eprint
 from pydub import AudioSegment  # type: ignore
+import time
 from pydub.playback import play  # type: ignore
 
 
@@ -8,11 +9,14 @@ class Player:
     Plays wave file
     """
 
-    def __init__(self, wave_file_path: str):
+    def __init__(self, wave_file_path: str, player_delay: float):
         self.wave = AudioSegment.from_wav(wave_file_path)
+        self.player_delay = player_delay
         self.__log("Initialised successfully")
 
     def play(self):
+        # TODO make this the correct amount of time and set this as an argument/or time constant
+        time.sleep(self.player_delay)  # this is a compensation thing
         play(self.wave)
 
     def __log(self, msg: str):
